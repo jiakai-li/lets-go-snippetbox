b22029cac8a4ea9d412de6f58a416ff7f563ee8c
Jiakai Li <li.jiakai0305@gmail.com>
2024-12-27 15:49:47 +1300

    Chapter 2.9 Serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+
+	// Use mux.Handle() function to register the file server as the handler
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Register other handlers
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
@@ -16,4 +25,35 @@ func main() {
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+// Custom FileSystem for disabling directory listings
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		indexFile := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(indexFile); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
