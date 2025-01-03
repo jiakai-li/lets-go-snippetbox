dcf13c02be21c01616e7949e1da27d4808471b7d
lijia <li.jiakai0305@gmail.com>
2025-01-03 16:11:55 +1300

    Chapter 4.4 Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	// we need the driver’s init() function to run
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Hold the application-wide dependencies
@@ -15,6 +19,7 @@ type application struct {
 func main() {
 	// Parse command line argument
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Configure logger
@@ -29,6 +34,14 @@ func main() {
 		Level: slog.LevelDebug,
 	}))
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
 	// Initialize a new instance of application struct
 	app := &application{
 		logger: logger,
@@ -36,7 +49,22 @@ func main() {
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
